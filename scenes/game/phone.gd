3a6e5f5eb3f3fb83bd41cec459727eb31d67fe50
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-12 23:51:06 -0600

    Made phone.

--- scenes/game/phone.gd
+++ scenes/game/phone.gd
@@ -1,4 +1,7 @@
-extends Area2D
+class_name Phone extends Draggable
+
+@export var notif_pivot : Node2D
+@export var notif : Sprite2D
 
 func _enter_tree():
 	App.message_events_added.connect(show_notifs)
@@ -8,12 +11,21 @@ func _exit_tree():
 	App.message_events_done.disconnect(hide_notifs)
 
 func show_notifs():
-	pass
-func hide_notifs():
-	pass
+	var tw := notif.create_tween()
+	tw.tween_property(notif, "scale", Vector2.ONE, 0.2)
 
-func _input_event(viewport, event, shape_idx):
+func hide_notifs():
+	var tw := notif.create_tween()
+	tw.tween_property(notif, "scale", Vector2.ZERO, 0.2)
+
+func _ready():
+	super()
+	self.double_clicked.connect(open)
+
+func _process(delta):
+	notif_pivot.global_rotation = 0
+
+func open():
 	if !MessageView.current.visible:
-		if Input.is_action_just_released("click"):
-			MessageView.current.reveal()
-			get_viewport().set_input_as_handled()
+		MessageView.current.reveal()
+		get_viewport().set_input_as_handled()
